9251219c4d46d4ba7749ebcedba27b56296b21d6
devefe1 <61803264+devefe1@users.noreply.github.com>
2020-06-03 22:52:26 -0500

    adding to repo

---
+++ adviceapp_server/src/main/java/com/advice/adviceapp_server/AdviceappServerApplication.java
@@ -0,0 +1,13 @@
+package com.advice.adviceapp_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdviceappServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdviceappServerApplication.class, args);
+	}
+
+}
---
+++ adviceapp_server/src/test/java/com/advice/adviceapp_server/AdviceappServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.advice.adviceapp_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdviceappServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
